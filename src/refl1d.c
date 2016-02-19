034d0ba305868f179a8d2bebfb4cab16b4b8b5ea
Paul Kienzle <pkienzle@nist.gov>
2016-02-19 08:51:03 -0500

    code tidying

--- src/refl1d.c
+++ src/refl1d.c
@@ -215,6 +215,7 @@ ex_get_profile(fitinfo fit[], int k,
 int
 ex_ncalc(fitinfo fit[], int k)
 {
+  //return fit[k].dataA.n;
   return fit[k].nQ;
 }
 
@@ -222,15 +223,16 @@ void
 ex_get_reflectivity(fitinfo fit[], int k, int xs, Real Q[], Real R[])
 {
   int i;
-  for (i=0; i < fit[k].nQ; i++) Q[i] = fit[k].fitQ[i];
+  int nQ = ex_ncalc(fit, k);
+  for (i=0; i < nQ; i++) Q[i] = fit[k].fitQ[i];
   if (xs == 0) {
-    for (i=0; i < fit[k].nQ; i++) R[i] = fit[k].fitA[i];
+    for (i=0; i < nQ; i++) R[i] = fit[k].fitA[i];
   } else if (xs==1) {
-    for (i=0; i < fit[k].nQ; i++) R[i] = fit[k].fitB[i];
+    for (i=0; i < nQ; i++) R[i] = fit[k].fitB[i];
   } else if (xs==2) {
-    for (i=0; i < fit[k].nQ; i++) R[i] = fit[k].fitC[i];
+    for (i=0; i < nQ; i++) R[i] = fit[k].fitC[i];
   } else if (xs==3) {
-    for (i=0; i < fit[k].nQ; i++) R[i] = fit[k].fitD[i];
+    for (i=0; i < nQ; i++) R[i] = fit[k].fitD[i];
   }
 }
 
